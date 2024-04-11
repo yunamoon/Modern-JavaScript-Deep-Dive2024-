3f29f0bb4c9070074c692e45060ab533e86094ce
yunamoon <tpwkao0051@gmail.com>
2024-04-11 21:02:06 +0900

    weather widget complete

--- react-weather/src/components/Weather.jsx
+++ react-weather/src/components/Weather.jsx
@@ -1,70 +1,112 @@
-import { useRef, useState } from 'react';
-import clear from '../assets/clear.png';
-import cloud from '../assets/cloud.png';
-import drizzle from '../assets/drizzle.png';
-import humidity from '../assets/humidity.png';
-import rain from '../assets/rain.png';
-import search from '../assets/search.png';
-import snow from '../assets/snow.png';
-import wind from '../assets/wind.png';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 import './Weather.css'
+// icon
+import { WiHumidity, WiStrongWind } from 'react-icons/wi'
 
 const Weather = () => {
-    const [location , setLocation] = useState('');
-    const contentRef = useRef();
 
-    const weather_api_key = import.meta.env.VITE_WEATHER_API_KEY;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lon=kr&appid=${weather_api_key}&units=metric`
-    
-    const handleSearch = async () => {
+    // 선언
+    const contentRef = useRef();
+    const [location , setLocation] = useState('');
+    const [weatherData , setWeatherData] = useState({
+            location : "",
+            icon : "",
+            humidity : "",
+            temp : "",
+            wind : "",
+        }
+    );
+   
+
+    // 날씨 정보 
+    const getWeather = async (lat, lon) => {
+
+        const weather_api_key = import.meta.env.VITE_WEATHER_API_KEY;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lon=kr&appid=${weather_api_key}&units=metric`;
+       
+        if(lat, lon) {
+            url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=metric`
+        } 
+       
+        try {
+            let response = await axios.get(url);
+            let data = response.data;
+            setWeatherData({
+                location : data.name, 
+                icon : `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+                humidity : data.main.humidity, 
+                temp : data.main.temp,
+                wind : data.wind.speed,
+            });
+        } catch (error) {
+            console.log('에러 발생' , error)
+        }
+    };
+
+    // 검색 핸들러
+    const handleSearch = () => {
         if(location === "") {
             contentRef.current.focus();
             return;
         } 
-            let response = await fetch(url);
-            let data = await response.json();
-            console.log('데이타' , data);
-        
+        getWeather();
     };
 
+    // input 핸들러
     const handleInput = (e) => {
         setLocation(e.target.value);
     };
 
+   // input 엔터 처리
+    const onKeydown = (e) => {
+        if (e.keyCode === 13) {
+            getWeather();
+        }
+      };
+
+    // 마운트 시, 현재 위치 날씨 정보
+    useEffect(()=> {
+        navigator.geolocation.getCurrentPosition((position)=> {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+           getWeather(lat, lon);
+        })
+    }, []);
+
+
     return (
     <div className='container'>
+
         <div className='search-bar'>
             <input className='search-input' 
-            placeholder='원하는 도시를 검색하세요.' 
+            placeholder='원하는 도시를 검색하세요. 🥰' 
             ref={contentRef}
-            onChange={handleInput} />
-            <div className='search-icon' onClick={handleSearch}>
-                <img src={search} alt='search-icon'></img>
-            </div>
+            onChange={handleInput}
+            onKeyUp={onKeydown} />
+            <button className='search-icon' 
+            onClick={handleSearch}>
+                search
+            </button>
         </div>
 
-        <div className='weather-logo'>
-            <img src={cloud} alt='cloud-logo'/>
-        </div>
-       
-        <div className='weather-temp'>24℃</div>
-       
-        <div className='weather-location'>London</div>
+        <img className='weather-logo' src={weatherData.icon} alt='logo'/>
+        <h2 className='weather-temp'>{weatherData.temp}℃</h2>
+        <h3 className='weather-location'>{weatherData.location}</h3>
       
-        <div className='weather-data'>
-            
+        <div className='weather-data'>            
             <div className='element'>
-                <img src={humidity} alt='' className='icon'/>
+                <WiHumidity className='icon' size={80}/>
                 <div className='data'>
-                    <div className='humidity'>100%</div>
+                    <div className='humidity'>{weatherData.humidity}%</div>
                     <div className='label'>습도</div>
                 </div>
             </div>
 
             <div className='element'>
-                <img src={wind} alt='' className='icon'/>
+               <WiStrongWind className='icon' size={80}/>
                 <div className='data'>
-                    <div className='wind'>100 km/h</div>
+                    <div className='wind'>{weatherData.wind} km/h</div>
                     <div className='label'>풍속</div>
                 </div>
             </div>
